71581d262817dc6b3b8c173a740250bd9d98256a
ivxxi <crystalalice21@gmail.com>
2020-06-02 12:58:19 +0300

    removes login file, home file and section from views.py

--- views.py
+++ views.py
@@ -5,26 +5,9 @@ from app.requests import get_articles, get_sources, topheadlines
 
 
 app = Flask(__name__)
+
+
 @app.route('/')
-def hello_world():
-    return render_template("login.html")
-database={'user':'password','crystal':'1234','guest':'guest'}
-
-
-@app.route('/form_login',methods=['POST','GET'])
-def login():
-    name1=request.form['username']
-    pwd=request.form['password']
-    if name1 not in database:
-	    return render_template('login.html',info='Invalid User')
-    else:
-        if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
-        else:
-	         return render_template('home.html',name=name1)
-
-
-@app.route('/index')
 def index():
     '''
     View root page function that returns the index page and its data
